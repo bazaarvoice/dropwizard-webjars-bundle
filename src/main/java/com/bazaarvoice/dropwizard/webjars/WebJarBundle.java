cda9a5b2f52d1876681a6688d5c2e90d6bb58d7f
Brandon Beck <brandon.beck@bazaarvoice.com>
2013-06-03 22:04:03 -0500

    Disable gzip to prevent it from mucking with Etags

--- src/main/java/com/bazaarvoice/dropwizard/webjars/WebJarBundle.java
+++ src/main/java/com/bazaarvoice/dropwizard/webjars/WebJarBundle.java
@@ -2,14 +2,20 @@ package com.bazaarvoice.dropwizard.webjars;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
-import com.yammer.dropwizard.Bundle;
+import com.yammer.dropwizard.ConfiguredBundle;
 import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.config.GzipConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
 
-public class WebJarBundle implements Bundle {
+public class WebJarBundle implements ConfiguredBundle<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebJarBundle.class);
+
     private CacheBuilder cacheBuilder = null;
     private List<String> packages = Lists.newArrayList(WebJarServlet.DEFAULT_WEBJAR_PACKAGES);
 
@@ -35,7 +41,13 @@ public class WebJarBundle implements Bundle {
 
     @SuppressWarnings("unchecked")
     @Override
-    public void run(Environment environment) {
+    public void run(Configuration config, Environment environment) {
+        GzipConfiguration gzip = config.getHttpConfiguration().getGzipConfiguration();
+        if (gzip.isEnabled()) {
+            LOGGER.warn("Disabling gzip as it's incompatible with the WebJarBundle.");
+            gzip.setEnabled(false);
+        }
+
         WebJarServlet servlet = new WebJarServlet(cacheBuilder, packages);
         environment.addServlet(servlet, WebJarServlet.URL_PREFIX + "*");
     }
--- src/test/java/com/bazaarvoice/dropwizard/webjars/WebJarBundleTest.java
+++ src/test/java/com/bazaarvoice/dropwizard/webjars/WebJarBundleTest.java
@@ -1,15 +1,21 @@
 package com.bazaarvoice.dropwizard.webjars;
 
+import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.config.GzipConfiguration;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.RETURNS_MOCKS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class WebJarBundleTest {
+    private final Configuration configuration = mock(Configuration.class, RETURNS_DEEP_STUBS);
     private final Environment environment = mock(Environment.class);
 
     private WebJarServlet servlet;
@@ -23,8 +29,17 @@ public class WebJarBundleTest {
         assertEquals("/webjars/*", path);
     }
 
+    @Test
+    public void testDisablesGzip() {
+        GzipConfiguration gzip = configuration.getHttpConfiguration().getGzipConfiguration();
+        when(gzip.isEnabled()).thenReturn(true);
+
+        runBundle(new WebJarBundle());
+        verify(gzip).setEnabled(false);
+    }
+
     private void runBundle(WebJarBundle bundle) {
-        bundle.run(environment);
+        bundle.run(configuration, environment);
 
         ArgumentCaptor<WebJarServlet> servletCaptor = ArgumentCaptor.forClass(WebJarServlet.class);
         ArgumentCaptor<String> pathCaptor = ArgumentCaptor.forClass(String.class);
