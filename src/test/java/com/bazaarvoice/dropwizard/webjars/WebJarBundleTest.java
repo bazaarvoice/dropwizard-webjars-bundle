60c2b3be315cc2b456587ad3b01fda88a737d71d
Nathaniel Bauernfeind <Nathaniel.Bauernfeind@bazaarvoice.com>
2014-04-23 13:50:02 -0500

    Per PR Comments.

--- src/test/java/com/bazaarvoice/dropwizard/webjars/WebJarBundleTest.java
+++ src/test/java/com/bazaarvoice/dropwizard/webjars/WebJarBundleTest.java
@@ -3,9 +3,12 @@ package com.bazaarvoice.dropwizard.webjars;
 import io.dropwizard.setup.Environment;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
+
+import javax.servlet.ServletRegistration;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -17,8 +20,9 @@ public class WebJarBundleTest {
     public void testRegistersAtWebjarsPath() {
         new WebJarBundle().run(environment);
 
+        ServletRegistration.Dynamic dynamic = environment.servlets().addServlet(eq("webjars"), notNull(WebJarServlet.class));
         ArgumentCaptor<String> pathCaptor = ArgumentCaptor.forClass(String.class);
-        verify(environment.servlets().addServlet(Matchers.eq("webjars"), Matchers.notNull(WebJarServlet.class))).addMapping(pathCaptor.capture());
+        verify(dynamic).addMapping(pathCaptor.capture());
 
         assertEquals("/webjars/*", pathCaptor.getValue());
     }
